becbd4260fa5b028a5971ee229ae38bdfc69f47a
seung-yeol <oks153123@naver.com>
2018-04-20 15:29:35 +0900

    4.Connection의 Dependency를 client로 ... dependency injection

---
+++ src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -0,0 +1,12 @@
+package kr.ac.jejunu;
+
+public class DaoFactory {
+
+    public ProductDao getProductDao() {
+        return new ProductDao(getConnectionMaker());
+    }
+
+    public ConnectionMaker getConnectionMaker(){
+        return new HanllaConnectionMaker();
+    }
+}
